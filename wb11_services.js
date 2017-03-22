1c95cc62e3bbd31e72317f1d97e719852c0a4f69
Hardik Bansal <Hardik Bansal>
2017-03-22 19:18:58 +0530

    First commit

---
+++ wb11_services.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function()
+{var Carousel1Opts={delay:2500,duration:1000,easing:'linear',mode:'fade',direction:'',scalemode:3,pagination:true,pagination_img_default:'images/page_default.png',pagination_img_active:'images/page_active.png',start:1};$("#Carousel1").carouseleffects(Carousel1Opts);$("#Carousel1_back a").click(function()
+{$('#Carousel1').carouseleffects('prev');});$("#Carousel1_next a").click(function()
+{$('#Carousel1').carouseleffects('next');});function homeScroll()
+{var $obj=$("#wb_home");if(!$obj.hasClass("in-viewport")&&$obj.inViewPort(false))
+{$obj.addClass("in-viewport");ShowObject('StickyLayer',0);}
+if($obj.hasClass("in-viewport")&&!$obj.inViewPort(true))
+{$obj.removeClass("in-viewport");ShowObject('StickyLayer',1);}}
+homeScroll();$(window).scroll(function(event)
+{homeScroll();});$("a[href*='#home']").click(function(event)
+{event.preventDefault();$('html, body').stop().animate({scrollTop:$('#wb_home').offset().top},500,'easeOutCubic');});$("a[href*='#portfolio']").click(function(event)
+{event.preventDefault();$('html, body').stop().animate({scrollTop:$('#wb_portfolio').offset().top},600,'easeOutCubic');});$("a[href*='#contact']").click(function(event)
+{event.preventDefault();$('html, body').stop().animate({scrollTop:$('#wb_contact').offset().top},600,'easeOutCubic');});$("#StickyLayer").stickylayer({orientation:3,position:[50,50],delay:0});});
