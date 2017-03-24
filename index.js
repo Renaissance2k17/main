bb72ee5cb82c2145c7a639a79bf7f31bfb9cd93c
Hardik Bansal <Hardik Bansal>
2017-03-24 23:33:37 +0530

    Signed-off-by: Hardik Bansal <Hardik Bansal>

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function()
-{var Carousel1Opts={delay:2500,duration:1000,easing:'linear',mode:'fade',direction:'',scalemode:3,pagination:true,pagination_img_default:'images/page_default.png',pagination_img_active:'images/page_active.png',start:1};$("#Carousel1").carouseleffects(Carousel1Opts);$("#Carousel1_back a").click(function()
+{$("a[href*='#LayoutGrid6']").click(function(event)
+{event.preventDefault();$('html, body').stop().animate({scrollTop:$('#wb_LayoutGrid6').offset().top},600,'linear');});var Carousel1Opts={delay:2500,duration:1000,easing:'linear',mode:'fade',direction:'',scalemode:3,pagination:true,pagination_img_default:'images/page_default.png',pagination_img_active:'images/page_active.png',start:1};$("#Carousel1").carouseleffects(Carousel1Opts);$("#Carousel1_back a").click(function()
 {$('#Carousel1').carouseleffects('prev');});$("#Carousel1_next a").click(function()
 {$('#Carousel1').carouseleffects('next');});$("a[href*='#LayoutGrid2']").click(function(event)
 {event.preventDefault();$('html, body').stop().animate({scrollTop:$('#wb_LayoutGrid2').offset().top},600,'linear');});function homeScroll()
